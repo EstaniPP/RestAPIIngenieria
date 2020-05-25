a32ceb4160767edf5c6f9dc23fca680a3c7b1c62
Maximo Librandi <maximolibrandi@hotmail.com>
2020-05-25 18:51:05 -0300

    ExercisesController and PhoneNumberController added

---
+++ src/controllers/exerciseController.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+
+const mysqlConnection = require('../database');
+
+router.get('/exercises/', (req, res) => {
+    mysqlConnection.query('SELECT * FROM Exercises', (err, rows, fields) => {
+        if(!err){
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.get('/exercises/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('SELECT * FROM Exercises WHERE id = ?', [id], (err, rows, fields) => {
+        if(!err){
+            res.json(rows[0]);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.post('/exercises/', (req, res) => {
+    const { description, path } = req.body;
+    const query = 'INSERT INTO Exercises(description, path) VALUES (?,?)';
+    mysqlConnection.query(query, [description, path], (err, rows, fields) => {
+        if(!err){
+            res.json({Status: 'Exercise saved'});
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.put('/exercises/:id', (req, res) => {
+    const { id } = req.params;
+    const { description, path } = req.body;
+    const query = 'UPDATE Exercises SET description = ?, path = ? WHERE id = ? ';
+    mysqlConnection.query(query, [description, path, id], (err, rows, fields) => {
+        if(!err){
+            res.json({Status: 'Exercise updated'});
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.delete('/exercises/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('DELETE FROM Exercises WHERE id = ?', [id], (err, rows, fields) => {
+        if(!err){
+            res.json({Status: 'Exercise deleted'});
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+module.exports = router;
---
+++ src/controllers/phoneNumberController.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+
+const mysqlConnection = require('../database');
+
+router.get('/phoneNumbers/', (req, res) => {
+    mysqlConnection.query('SELECT * FROM Phone_Numbers', (err, rows, fields) => {
+        if(!err){
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.get('/phoneNumbers/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('SELECT * FROM Phone_Numbers WHERE id = ?', [id], (err, rows, fields) => {
+        if(!err){
+            res.json(rows[0]);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.post('/phoneNumbers/', (req, res) => {
+    const { user_id, number } = req.body;
+    const query = 'INSERT INTO Phone_Numbers(user_id, number) VALUES (?,?)';
+    mysqlConnection.query(query, [user_id, number], (err, rows, fields) => {
+        if(!err){
+            res.json({Status: 'Phone number saved'});
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.put('/phoneNumbers/:id', (req, res) => {
+    const { id } = req.params;
+    const { user_id, number } = req.body;
+    const query = 'UPDATE Phone_Numbers SET user_id = ?, number = ? WHERE id = ? ';
+    mysqlConnection.query(query, [user_id, number, id], (err, rows, fields) => {
+        if(!err){
+            res.json({Status: 'Phone number updated'});
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.delete('/phoneNumbers/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('DELETE FROM Phone_Numbers WHERE id = ?', [id], (err, rows, fields) => {
+        if(!err){
+            res.json({Status: 'Phone number deleted'});
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+module.exports = router;
