708f442a4a15285bfba8ecb40cdc754aa0da82c9
Valentin <valentinfvazquez@gmail.com>
2020-05-28 15:11:36 -0300

    MercadoPago integration working

--- src/controllers/mpController.js
+++ src/controllers/mpController.js
@@ -20,11 +20,9 @@ const verifyTokenUser = require('./verifyTokenUser');
         Body format:
             - workoutList: []
 */
+
+
 router.post('/mercadop/', verifyTokenUser, (req, res) => {
-    mercadopago.configure({
-        sandbox: true,
-        access_token: "TEST-7376157659045255-052717-375fef09302638cd785342590cc90682-53060878"
-    });
     // workout list
     const workoutList = req.body.workoutList;
     // set preferences for MercadoPago API
@@ -38,77 +36,75 @@ router.post('/mercadop/', verifyTokenUser, (req, res) => {
     // get user information
     mysqlConnection.query('SELECT * FROM Users WHERE id = ?', [req.id], async (err, rows) => {
         payerInfo = rows[0];
+        // get al workouts and description
+        mysqlConnection.query('SELECT * FROM Workouts WHERE id IN (?)', [workoutList], async (err, rows) => {
+            rows.map((elem) => {
+                items.push({
+                    title: elem.id.toString(), // replace for workout_title
+                    quantity: 1,
+                    currency_id: 'ARS',
+                    unit_price: elem.workout_price
+                });
+            });
+            // check if there is at least a row to work with.
+            // let's assume front end retards are gonna send us workouts from only one medical personnel
+            if(items.length === 0){
+                // bad request
+                return res.status(400).json({
+                    error: 400,
+                    msg: "No existen workouts con el ID seleccionado"
+                });
+            }else{
+                doctorId = rows[0].medical_personnel_id;
+                // get medical user token and access key out of a workout
+                let credentials = null;
+                if(doctorId){
+                    mysqlConnection.query('SELECT * FROM Medical_Personnel WHERE id = ?', [doctorId], async (err, rows) => {
+                        credentials = {
+                            mp_access_token: rows[0].mp_access_token,
+                            mp_public_key: rows[0].mp_public_key
+                        };
+                        mercadopago.configure({
+                            sandbox: true,
+                            access_token: credentials.mp_access_token
+                        });
+                        // set payer's info
+                        payer = {
+                            name: payerInfo.name, 
+                            surname: payerInfo.last_name,
+                            email: payerInfo.email
+                        }
+                        // give attributes to API
+                        preference.items = items;
+                        preference.payer = payer;
+                        // Create the preference 
+                        mercadopago.preferences.create(preference).then(function (data) {
+                            return res.status(200).json({result: data});
+                        }).catch(function (error) {
+                            return res.status(500).json({error: error});
+                        }).finally(function(){
+                            mercadopago.configurations.setAccessToken(oldAccessToken);
+                        }); 
+                    }).on('error', function(error){
+                        console.log("[mysql error]", error);
+                        throw error;
+                    }); 
+                }else{
+                    // bad request
+                    return res.status(400).json({
+                        error: 400,
+                        msg: "No existe medico"
+                    });
+                }
+            }
+        }).on('error', function(error){
+            console.log("[mysql error]", error);
+            throw error;
+        });
     }).on('error', function(error){
         console.log("[mysql error]", error);
         throw error;
-    });
-    // get al workouts and description
-    workoutList.forEach(element => {
-        await mysqlConnection.query('SELECT * FROM Workouts WHERE  id = ?', [element], async (err, rows) => {
-            if(rows.length !== 0){
-                if(!doctorId){
-                    doctorId = rows[0].medical_personnel_id;
-                    console.log("DOCTOR ID: ", doctorId);
-                }
-                items.push({
-                    title: rows[0].id, // replace for workout_title
-                    quantity: 1,
-                    currency_id: 'ARS',
-                    unit_price: rows[0].workout_price
-                }); 
-                console.log("Pusheando");
-            }else{
-                console.log("No se ha encontrado un medico")
-            }            
-        }).on('error', function(error){
-            console.log("[mysql error]", error);
-            throw error;
-        });          
-    });
-    console.log(items.length);
-    // check if there is at least a row to work with.
-    // let's assume front end retards are gonna send us workouts from only one medical personnel
-    if(items.length === 0){
-        // bad request
-        return res.status(400).json({
-            error: 400,
-            msg: "No existen workouts con el ID seleccionado"
-        });
-    }
-    // get medical user token and access key out of a workout
-    let credentials = null;
-    if(doctorId){
-        mysqlConnection.query('SELECT * FROM Medical_Personnel WHERE id = ?', [doctorId], async (err, rows) => {
-            credentials = {
-                mp_access_token: rows[0].mp_access_token,
-                mp_public_key: rows[0].mp_public_key
-            };
-        }).on('error', function(error){
-            console.log("[mysql error]", error);
-            throw error;
-        }); 
-    }else{
-        // bad request
-        return res.status(400).json({
-            error: 400,
-            msg: "No existe medico"
-        });
-    }
-    // set payer's info
-    payerInfo = {
-        name: payerInfo.name, 
-        surname: payerInfo.last_name,
-        email: payerInfo.email
-    }
-    // give attributes to API
-    preference.items = items;
-    preference.payer = payerInfo;
-    // Create the preference
-    mercadopago.preferences.create(preference).then(function (data) {
-        return res.status(200).json({precerence: data});
-    }).catch(function (error) {
-        return res.status(500).json({error: error});
-    });        
+    });       
 });
 
 
