ba610293b8f460dd0278c6a2234ec7ef02863c4a
Valentin <valentinfvazquez@gmail.com>
2020-05-30 15:28:08 -0300

    Forgotten password feature added

--- src/controllers/passwordController.js
+++ src/controllers/passwordController.js
@@ -34,33 +34,34 @@ router.post('/forgot', (req, res) => {
                 });;
                 const salt = await bcrypt.genSalt(10);
                 const encNewPass = await bcrypt.hash(newPass, salt);
-                // set the user password
-                mysqlConnection.query('UPDATE Users SET password = ? WHERE email = ?', [encNewPass, accountEmail], async (err, result) => {
-                    // send email
-                    var transporter = nodemailer.createTransport({
-                        service: 'gmail',
-                        auth: {
-                          user: 'backendingenieria@gmail.com',
-                          pass: 'EstaNoEsLaPassword'
-                        }
-                    });
-                    var mailOptions = {
-                        from: 'backendingenieria@gmail.com',
-                        to: accountEmail,
-                        subject: 'Cambio de Contrasena',
-                        text: 'Su nueva contrasena es: ' + newPass
-                    };
-                    transporter.sendMail(mailOptions, function(error, info){
-                        if (error) {
-                          console.log(error);
-                        } else {
-                          console.log('Email sent: ' + info.response);
-                          return res.status(200).json({
-                            msg: "OK"
-                          });
-                        }
-                    });
+                // send email
+                var transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                      user: 'backendingenieria@gmail.com',
+                      pass: 'null'
+                    }
                 });
+                var mailOptions = {
+                    from: 'backendingenieria@gmail.com',
+                    to: accountEmail,
+                    subject: 'Cambio de Contrasena',
+                    text: 'Su nueva contrasena es: ' + newPass
+                };
+                transporter.sendMail(mailOptions, function(error, info){
+                    if (error) {
+                         console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                        // set the user password
+                        mysqlConnection.query('UPDATE Users SET password = ? WHERE email = ?', [encNewPass, accountEmail], async (err, result) => {
+                            return res.status(200).json({
+                                msg: "OK"
+                            });
+                        });
+                    }
+                });
+                
             }else{
                 // if email does not exist i'll send a 200 status code anyway
                 return res.status(500).json({
