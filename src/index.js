4ea6c482df820596bb6838b6ff7be907898e8ce6
Valentin <valentinfvazquez@gmail.com>
2020-06-05 11:52:35 -0300

    db connection fixed

--- src/index.js
+++ src/index.js
@@ -7,13 +7,22 @@ app.set('port',process.env.PORT || 3000);
 // Middlewares
 app.use(express.json());
 
+
 // Routes
 //LogIn y registrarse
 app.use(require('./controllers/authController'));
-//Controllers generales
-app.use(require('./controllers/usersJoinsController'));
+
+// MercadoPago services
+app.use(require('./controllers/mpController'));
+// MercadoPago services
+app.use(require('./controllers/passwordController'));
+
 app.use(require('./controllers/cityController'));
 app.use(require('./controllers/countryController'));
+/*
+//Controllers generales
+app.use(require('./controllers/usersJoinsController'));
+
 //app.use(require('./controllers/deviceUserController'));
 app.use(require('./controllers/diseaseController'));
 app.use(require('./controllers/documentTypeController'));
@@ -31,11 +40,8 @@ app.use(require('./controllers/userDiseaseController'));
 app.use(require('./controllers/workoutController'));
 app.use(require('./controllers/workoutExerciseController'));
 app.use(require('./controllers/workoutReportController'));
-// MercadoPago services
-app.use(require('./controllers/mpController'));
-// MercadoPago services
-app.use(require('./controllers/passwordController'));
 //Starting server
+*/
 app.listen(app.get('port'), () => {
     console.log('Server andando en port');
 });
