5168bfebfc71582293fb8d412e1f98f4fc846a9d
Estanipp <estanipp@gmail.com>
2020-06-15 18:17:04 -0300

    verify token error related to forbidden access fixed

--- src/controllers/verifyTokenMedical.js
+++ src/controllers/verifyTokenMedical.js
@@ -14,18 +14,18 @@ async function verifyTokenMedical(req, res, next) {
             mysqlConnection.query('SELECT * FROM Users WHERE email = ?', [decoded.id], async (err, rows, fields) => {
                 if (!err) {
                     user = rows[0];
-                    req.user_id = rows[0].id;
                     if(!user) {
                         return res.status(402).send();
                     }else{
+                        req.user_id = rows[0].id;
                         const user_id = rows[0].id;
                         mysqlConnection.query('SELECT * FROM Medical_Personnel WHERE user_id = ?', [user_id], async (err, rows, fields) => {
                             if (!err) {
                                 user = rows[0];
-                                req.id = rows[0].id;
                                 if(!user) {
                                     return res.status(403).send();
                                 }else{
+                                    req.id = rows[0].id;
                                     next();
                                 }
                             }else {
--- src/controllers/verifyTokenUser.js
+++ src/controllers/verifyTokenUser.js
@@ -12,18 +12,18 @@ async function verifyTokenUser(req, res, next) {
         mysqlConnection.query('SELECT * FROM Users WHERE email = ?', [decoded.id], async (err, rows, fields) => {
             if (!err) {
                 User = rows[0];
-                req.user_id = rows[0].id;
                 if(!User) {
                     return res.status(402).send();
                 }else{
+                    req.user_id = rows[0].id;
                     const User_id = rows[0].id;
                     mysqlConnection.query('SELECT * FROM Device_Users WHERE user_id = ?', [User_id], async (err, rows, fields) => {
                         if (!err) {
                             User = rows[0];
-                            req.id = rows[0].id;
                             if(!User) {
                                 return res.status(403).send();
                             }else{
+                                req.id = rows[0].id;
                                 next();
                             }
                         }else {
